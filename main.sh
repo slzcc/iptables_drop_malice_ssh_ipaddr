7b082f341adea781ba07db7c8b3e4aaf017eae94
shilei <shilei1@hydsoft.com>
2017-11-22 20:50:49 +0800

    add *

--- main.sh
+++ main.sh
@@ -1,31 +1,47 @@
 #!/bin/bash
-count1=`cat /root/iptables_drop_malice_ssh_ipaddr/malice_ssh_list.txt|wc -l`
+
+count1=`cat /root/iptables_drop_malice_ssh_ipaddr/malice_ssh_list.txt | wc -l`
 count2=0
 ss=""
 AllPort="0:65535"
 IPT="/sbin/iptables"
 
-for i in `cat /root/iptables_drop_malice_ssh_ipaddr/error_ssh.txt|sort|uniq`; do
+for i in `cat /root/iptables_drop_malice_ssh_ipaddr/error_ssh.txt | sort | uniq`; do
+
   let count2+=1
 
-  for j in `cat /root/iptables_drop_malice_ssh_ipaddr/accept_account.txt|sort|uniq`; do
+  for j in `cat /root/iptables_drop_malice_ssh_ipaddr/accept_account.txt | sort | uniq`; do
+
     if [ "$j" == "$i" ]; then
+
       echo "Remove $j Rule!"
+
       break
+
     fi
 
     if [ "${count1}" == "${count2}" ]; then
+
       ss+="$i"
+
     else
+
       ss+="$i",
+
     fi
+
   done
 
 done
 
 if [ "$1" == "start" ]; then
+
   $IPT -t filter -A INPUT -p tcp -s ${ss} --sport ${AllPort} --dport 22 -j DROP
+
 elif [ "$1" == "stop" ]; then
+
   $IPT -t filter -D INPUT -p tcp -s ${ss} --sport ${AllPort} --dport 22 -j DROP
+
 fi
+
 echo $1 Configure iptables!
