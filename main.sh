868c32185cf5ea3085d0bf83957022d60484d032
root <root@shileizcc.com>
2018-03-27 21:46:46 +0800

    edit main.sh

--- main.sh
+++ main.sh
@@ -1,43 +1,40 @@
 #!/bin/bash
 
 workDir="/root"
-sourceCount=`cat ${workDir}/iptables_drop_malice_ssh_ipaddr/malice_ssh_list.txt | wc -l`
-targetCount=0
-sourceIPList=`cat ${workDir}/iptables_drop_malice_ssh_ipaddr/malice_ssh_list.txt | sort | uniq`
-AddIPList=""
-DeleteIPList=""
+whiteList=`cat ${workDir}/iptables_drop_malice_ssh_ipaddr/neglect_ssh_list.txt | sort | uniq`
+blackList=`cat ${workDir}/iptables_drop_malice_ssh_ipaddr/malice_ssh_list.txt | sort | uniq`
+BlackIPList=""
+WhiteIPList=""
 AllPorts="0:65535"
 IPT="/sbin/iptables"
 suffix="(,$)"
 
-for i in ${sourceIPList}; do
+for i in ${blackList}; do
 
-  let targetCount+=1
-
-  AddIPList+="$i",
+  BlackIPList+="$i",
 
 done
 
-for j in ${sourceIPList}; do
+for j in ${whiteList}; do
 
-  DeleteIPList+="$j",
+  WhiteIPList+="$j",
 
 done
 
-[[ "$AddIPList" =~ $suffix ]] && AddIPList=${AddIPList%?}
+[[ "$BlackIPList" =~ $suffix ]] && BlackIPList=${BlackIPList%?}
 
-[[ "$DeleteIPList" =~ $suffix ]] && DeleteIPList=${DeleteIPList%?}
+[[ "$WhiteIPList" =~ $suffix ]] && WhiteIPList=${WhiteIPList%?}
 
 if [ "$1" == "start" ]; then
 
-  $IPT -t filter -A INPUT -p tcp -s ${DeleteIPList} --sport ${AllPorts} --dport 22 -j ACCEPT
-  $IPT -t filter -A INPUT -p tcp -s ${AddIPList} --sport ${AllPorts} --dport 22 -j DROP
+  $IPT -t filter -A INPUT -p tcp -s ${WhiteIPList} --sport ${AllPorts} --dport 22 -j ACCEPT
+  $IPT -t filter -A INPUT -p tcp -s ${BlackIPList} --sport ${AllPorts} --dport 22 -j DROP
 
 
 elif [ "$1" == "stop" ]; then
 
-  $IPT -t filter -D INPUT -p tcp -s ${DeleteIPList} --sport ${AllPorts} --dport 22 -j ACCEPT
-  $IPT -t filter -D INPUT -p tcp -s ${AddIPList} --sport ${AllPorts} --dport 22 -j DROP
+  $IPT -t filter -D INPUT -p tcp -s ${WhiteIPList} --sport ${AllPorts} --dport 22 -j ACCEPT
+  $IPT -t filter -D INPUT -p tcp -s ${BlackIPList} --sport ${AllPorts} --dport 22 -j DROP
 
 fi
 
